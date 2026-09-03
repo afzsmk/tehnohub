2c9374e98bfa6b4b2f298dccc6d8ebaedc1e9318
afzsmk <af.zsmk@gmail.com>
2026-09-03 15:33:59 +0300

    Update vite.config.ts

--- workforce/vite.config.ts
+++ workforce/vite.config.ts
@@ -1,11 +1,15 @@
+/// <reference types="vitest" />
 import { defineConfig } from 'vite';
 
 export default defineConfig({
-  // Базовый путь для GitHub Pages (замените при необходимости)
   base: './',
   build: {
     outDir: 'dist',
     assetsDir: 'assets',
     sourcemap: false
+  },
+  test: {
+    globals: true,
+    environment: 'node'
   }
 });
