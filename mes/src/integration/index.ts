29df7573d1b9c7aab8d3592a0357e47160660782
afzsmk <af.zsmk@gmail.com>
2026-09-08 15:58:41 +0300

    feat(mes): export server replan client

--- mes/src/integration/index.ts
+++ mes/src/integration/index.ts
@@ -3,4 +3,5 @@ export * from './identitySession';
 export * from './auth';
 export * from './mesExecutionRpc';
 export * from './mesPlanningRpc';
+export * from './mesReplanRpc';
 export * from './workforce';
