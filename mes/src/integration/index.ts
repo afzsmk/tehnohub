f6612b16305630ad5cc199f4f25b5786d834c68b
afzsmk <af.zsmk@gmail.com>
2026-09-08 15:55:27 +0300

    feat(mes): export planning RPC client

--- mes/src/integration/index.ts
+++ mes/src/integration/index.ts
@@ -2,4 +2,5 @@ export * from './identity';
 export * from './identitySession';
 export * from './auth';
 export * from './mesExecutionRpc';
+export * from './mesPlanningRpc';
 export * from './workforce';
