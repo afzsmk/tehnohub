f3de7b0a9784b98a6357b80af1a31b2b157e46c2
afzsmk <af.zsmk@gmail.com>
2026-09-10 10:33:19 +0300

    feat(workforce): export MES publication service

--- workforce/src/integration/mes/index.ts
+++ workforce/src/integration/mes/index.ts
@@ -4,11 +4,20 @@ export {
   nextPublicationVersion
 } from './publication';
 
+export { publishPlanForMes, publishPlanForMesInState, ensureMesPlanId } from './publisher';
+
 export type {
   PublishedPlanStatus,
   WorkforceProductRef,
   WorkforceProfessionRef,
   WorkforceMonthlyPlanItem,
   WorkforcePublishedPlanDto,
+  MesImportReceipt,
   MesPublicationContext
 } from './publication';
+
+export type {
+  MesPlanPublisher,
+  MesPublicationResult,
+  MesPublicationOptions
+} from './publisher';
