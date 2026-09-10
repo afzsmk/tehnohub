271a3bc1f7e96fea1b4ce2ce5a6b3e9a7fec2c2f
afzsmk <af.zsmk@gmail.com>
2026-09-10 10:26:35 +0300

    feat(workforce): export MES publication contract

---
+++ workforce/src/integration/mes/index.ts
@@ -0,0 +1,14 @@
+export {
+  WORKFORCE_MES_CONTRACT_VERSION,
+  buildPublishedPlanDto,
+  nextPublicationVersion
+} from './publication';
+
+export type {
+  PublishedPlanStatus,
+  WorkforceProductRef,
+  WorkforceProfessionRef,
+  WorkforceMonthlyPlanItem,
+  WorkforcePublishedPlanDto,
+  MesPublicationContext
+} from './publication';
