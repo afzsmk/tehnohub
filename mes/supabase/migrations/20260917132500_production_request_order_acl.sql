80b742c36ae1ba6fe9b89581e94ac003bc608b87
afzsmk <af.zsmk@gmail.com>
2026-09-17 13:23:19 +0300

    MES: lock down production request order RPC execution

---
+++ mes/supabase/migrations/20260917132500_production_request_order_acl.sql
@@ -0,0 +1,2 @@
+revoke all on function mes_create_production_order_from_request_item(text,text,text,text,timestamptz,text) from public, anon;
+grant execute on function mes_create_production_order_from_request_item(text,text,text,text,timestamptz,text) to authenticated;
